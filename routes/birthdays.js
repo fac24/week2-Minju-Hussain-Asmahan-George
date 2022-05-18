e8ca149048155e822b1baa8fcb82cc142e537366
glombort <george.lambert99@gmail.com>
2022-05-18 17:10:04 +0100

    Added list of pets with dates
    Doesn't look that nice styling wise but we have access to the data!!
    
    Co-Authored-By: Hussain Jawad <45372216+HusJAW@users.noreply.github.com>

--- routes/birthdays.js
+++ routes/birthdays.js
@@ -1,9 +1,48 @@
-//const db = require("../database/connection.js");
+const db = require("../database/connection.js");
 
 function get(request, response) {
-    const html = /* html */
-    `<h1>birthdays page</h1>`;
-    response.send(html);
+    const htmlHead = /* html */ `
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>PET Birthdays</title>
+      <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+  `;
+
+    const selectPets = /* sql */ `
+        SELECT pets.pet_name, pets.birth_date, pet_type.pet_kind
+        FROM pets
+            INNER JOIN pet_type ON pets.type_id = pet_type.id;
+    `
+    db.query(selectPets).then((result) => {
+        const names = result.rows;
+        const nameList = names.map( name => {
+           return /* html */ `
+            <li>
+                Hiya ${name.pet_name}, you are a ${name.pet_kind}, your birthday is ${name.birth_date}
+            </li>`
+        })
+        
+        const htmlBody = /* html */ `
+            <body>
+                <main>
+                    <ul>${nameList.join("")}</ul>
+                </main>
+            </body>
+        `;
+
+        const html = /* html */`
+            <!doctype html>
+            <html lang="en">
+                ${htmlHead}
+                ${htmlBody}
+            </html>
+        `;
+        response.send(html);
+    })
+
 };
 
 module.exports = { get };
